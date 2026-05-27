84abe85fe866364e1cde0c44b0c2e8423410df10
biomoleculaire7-commitsb <biomoleculaire7@gmail.com>
2026-05-27 10:53:16 +0200

    change map to light theme

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -50,7 +50,7 @@ export default function MapView({ infections, heatmapData, mapMode, setMapMode, 
       zoomControl: true,
     })
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '© OpenStreetMap © CARTO',
       subdomains: 'abcd',
       maxZoom: 19,
